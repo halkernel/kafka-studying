22f6b3c344e57e956105e7e305eec91b1150797a
Kennt Calixto <kennet.emerson@gmail.com>
2020-07-01 01:11:56 -0300

    kafka testing callback

--- src/main/java/com/github/halkernel/producer/ProducerDemo.java
+++ src/main/java/com/github/halkernel/producer/ProducerDemo.java
@@ -1,9 +1,9 @@
 package com.github.halkernel.producer;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import java.util.Properties;
@@ -13,6 +13,7 @@ public class ProducerDemo {
 
     public static void main(String[] args) {
 
+        final Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
 
         String bootstrapServer = "127.0.0.1:9092";
         String message = String.format("hello from %s", ProducerDemo.class.getSimpleName());
@@ -33,8 +34,24 @@ public class ProducerDemo {
         ProducerRecord<String, String> producerRecord
                 = new ProducerRecord<String, String>("first_topic", message);
 
-        //send data
-        producer.send(producerRecord);
+        //send data - asynchronously
+        producer.send(producerRecord, new Callback() {
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                //execute every time a record is successfully sent or an exception is thrown
+                if(e == null){
+                    //the record was sent
+                    logger.info(
+                            String.format("\n Received new metadata -- \n Topic: %s \n Partition: %s \n Offset: %s \n Timestamp: %s \n",
+                                        recordMetadata.topic(),
+                                        recordMetadata.partition(),
+                                        recordMetadata.offset(),
+                                        recordMetadata.timestamp())
+                    );
+                }else{
+                    logger.error("Error while producing", e);
+                }
+            }
+        });
 
         //flush and close producer
         producer.close();
