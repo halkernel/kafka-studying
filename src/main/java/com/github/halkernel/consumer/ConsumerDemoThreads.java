c24009030771c4d808f2feec55b03f98b9859135
Kennt Calixto <kennet.emerson@gmail.com>
2020-07-22 02:03:09 -0300

    consumers with threads

---
+++ src/main/java/com/github/halkernel/consumer/ConsumerDemoThreads.java
@@ -0,0 +1,77 @@
+package com.github.halkernel.consumer;
+
+import com.github.halkernel.thread.ConsumerThread;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class ConsumerDemoThreads {
+
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ConsumerDemoThreads.class);
+        final String bootstrapServers = "127.0.0.1:9092";
+        final String groupId = "testing-sixth-application";
+        final String topic = "first_topic";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); //"earliest" "latest" or "none"
+
+        //create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        //latch for doung with multiple threads
+        CountDownLatch latch = new CountDownLatch(1);
+
+        //consumer runnable is created
+        Runnable consumerThread = new ConsumerThread(latch, properties, topic);
+
+        //starts the thread
+        Thread thread = new Thread(consumerThread);
+        thread.start();
+
+        //add shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutdown Hook is Being Called");
+            ((ConsumerThread) consumerThread).dropConsumer();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                logger.info("Application has exited");
+            }
+        }));
+
+        //output should be:
+        /*
+        *
+        * [Thread-2] INFO com.github.halkernel.consumer.ConsumerDemoThreads - Shutdown Hook is Being Called
+        * [Thread-1] INFO com.github.halkernel.thread.ConsumerThread - Received shutdown signal!
+        * [main] INFO com.github.halkernel.consumer.ConsumerDemoThreads - Application is down
+        * [Thread-2] INFO com.github.halkernel.consumer.ConsumerDemoThreads - Application has exited
+        * */
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            logger.error("Application interrupted", e);
+        }finally {
+            logger.info("Application is closing");
+        }
+
+    }
+}
---
+++ src/main/java/com/github/halkernel/thread/ConsumerThread.java
@@ -0,0 +1,54 @@
+package com.github.halkernel.thread;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class ConsumerThread implements Runnable{
+
+    private Logger logger = LoggerFactory.getLogger(ConsumerThread.class.getName());
+
+    private CountDownLatch latch;
+    private KafkaConsumer<String, String> consumer;
+
+    public ConsumerThread(CountDownLatch latch,
+                          Properties properties,
+                          String topic){
+        this.latch = latch;
+        consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Collections.singleton(topic));
+    }
+
+    @Override
+    public void run() {
+        try{
+            while (true){ //just for understanding
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord record: consumerRecords) {
+                    logger.info("Key: " + record.key() + " " + "Value: " + record.value());
+                    logger.info("Partition: " + record.partition());
+                    logger.info("Offset: " + record.offset());
+                }
+            }
+        }catch (WakeupException ex){
+            logger.info("Received shutdown signal!");
+        }finally {
+            consumer.close();
+            latch.countDown();
+        }
+    }
+
+    public void dropConsumer(){
+        //this method interrupt the consumer.poll()
+        //it will throw the WakeupException
+        consumer.wakeup();
+    }
+}
