d45e085c6ca9e40b0af24b10d6cf4160e25b9173
Kennt Calixto <kennet.emerson@gmail.com>
2020-08-15 02:34:37 -0300

    idempotence

---
+++ kafka-consumer-elasticsearch/src/main/java/consumer/ElasticSearch.java
@@ -0,0 +1,66 @@
+package consumer;
+
+import com.google.gson.JsonParser;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+public class ElasticSearch {
+
+    public static void main(String[] args) throws IOException {
+
+        Logger logger = LoggerFactory.getLogger(ElasticSearch.class.getName());
+
+        RestHighLevelClient client = RestHighLevelClientCreator.createClient();
+
+        KafkaConsumer<String, String> consumer = RestHighLevelClientCreator.createConsumer("twitter_tweets");
+        while (true) { //just for understanding, not recommendable
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : consumerRecords) {
+
+                //there are 2 strategies for creating ids
+                // 1 kafka generic id:
+                //String id = record.topic() + "_" + record.partition() + "_" + record.offset();
+
+                // 2 twitter feed specific id
+                String id = extractIdFromTweet(record.value());
+
+                IndexRequest indexRequest = new IndexRequest(
+                        "twitter",
+                        "tweets",
+                        id // this is to make our consumer idempotent
+                ).source(record.value().getBytes(StandardCharsets.US_ASCII), XContentType.JSON);
+
+                IndexResponse response = client.index(indexRequest, RequestOptions.DEFAULT);
+                String responseId = response.getId();
+                logger.info(responseId);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //client.close();
+    }
+
+    private static String extractIdFromTweet(String tweet){
+        JsonParser jsonParser = new JsonParser();
+        return jsonParser.parse(tweet)
+                .getAsJsonObject()
+                .get("id_str")
+                .getAsString();
+    }
+}
---
+++ kafka-consumer-elasticsearch/src/main/java/consumer/RestHighLevelClientCreator.java
@@ -0,0 +1,82 @@
+package consumer;
+
+import config.TokenConfig;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class RestHighLevelClientCreator {
+
+    private static String host = "";
+    private static String user = "";
+    private static String pass = "";
+
+    public static RestHighLevelClient createClient(){
+
+        setValues();
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(user,pass));
+
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(host, 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+    }
+
+    public static KafkaConsumer<String, String> createConsumer(String topic){
+        final String bootstrapServers = "127.0.0.1:9092";
+        final String groupId = "kafka-demo-elasticsearch";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); //"earliest" "latest" or "none"
+
+        //create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+    }
+
+    private static void setValues() {
+        TokenConfig tokenConfig= new TokenConfig();
+
+        Properties properties = null;
+        try {
+            properties = tokenConfig.getProperties();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        host = properties.getProperty("elasticSearchUri");
+        user = properties.getProperty("elasticAccessKey");
+        pass = properties.getProperty("elasticAccessSecret");
+
+    }
+
+
+}
